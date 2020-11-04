2547acf7518073c32411b9f1fe8f14dac14c8a13
Samuel da Cruz <44683435+samuel-cruz@users.noreply.github.com>
2020-11-04 09:13:50 -0300

    add service d

--- microsservicos-aula-1/c/c.go
+++ microsservicos-aula-1/c/c.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 type Coupon struct {
@@ -54,4 +55,33 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, string(jsonResult))
 
+	if(result.Status == "valid") {
+		result := sendEmail()
+		if(result.Status != "ok") {
+			fmt.Fprintf(w, result.Status)
+		}
+	}
+
+}
+
+func sendEmail() Result {
+	result := Result{Status: "ok"}
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 2
+
+	res, err := retryClient.PostForm("http://localhost:9093", nil)
+	if err != nil {
+		result := Result{Status: "Servidor de e-mail fora do ar!"}
+		fmt.Println(result.Status)
+		addOnQueue("email@domain.com")
+		return result
+	}
+
+	defer res.Body.Close()
+
+	return result
+}
+
+func addOnQueue(email string) {
+	fmt.Println(email)
 }
